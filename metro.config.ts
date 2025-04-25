ee75e97bccc4a8930d5d1b8cecd1c161d58f8cd5
Suchismit4 <Suchismit4@gmail.com>
2025-04-25 13:37:17 -0400

    Expo version type annot fix.

--- metro.config.ts
+++ metro.config.ts
@@ -1,10 +1,9 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 import { getDefaultConfig } from 'expo/metro-config';
-import { MetroConfig } from 'expo/metro-config';
 
 /**
  * Metro bundler configuration for the TikTik application
  */
-const config: MetroConfig = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
 export default config; 
